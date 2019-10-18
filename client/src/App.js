0079fd2da670e103e47681ef624b43a663c3b968
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-18 08:37:43 -0700

    Added Private Route, axiosWithAuth and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,43 @@
+// import React, { useState } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import Login from "./components/Login";
+// import "./styles.scss";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Route exact path="/" component={Login} />
+//         {/* 
+//           Build a PrivateRoute component that will 
+//           display BubblePage when you're authenticated 
+//         */}
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
+           {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/BubblesPage" component={BubblePage} />   
       </div>
     </Router>
   );
