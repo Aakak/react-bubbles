e716c7fe4e993196cd851022c2233b50d8a570ed
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-18 09:54:50 -0700

    Completed MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -10,21 +10,56 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
-
+  const [colorAddNew, setColorAddNew] = useState(initialColor);
+  
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
-    e.preventDefault();
+
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+
+  const saveEdit = e => {
+    console.log('save edit!');
+    e.preventDefault();
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, {color: colorToEdit.color, code: colorToEdit.code})
+    .then(res => {console.log(res)})
+    .catch(err => console.log(err.response));
+
+    setEditing(false);
+    const updatedColors = colors.map(color => {
+      if (color.id === colorToEdit.id) {
+        return {...color, color: colorToEdit.color, code: colorToEdit.code}
+      } else {
+        return color;
+      }
+    }) 
+    updateColors(updatedColors);   
   };
+   // make a delete request to delete this color
+     // // Make a put request to save your updated color
+    // // think about where will you get the id from...
+    // // where is is saved right now?
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    // e.preventDefault();
+   axiosWithAuth()
+    .delete(`/colors/${color.id}`)
+    .then(res => console.log(res))
+    .catch(err => console.log(err.response));
+
+
+    const remainingColors =[];
+    colors.forEach(color => {
+      if (color.id !== colorToEdit.id) {
+        remainingColors.push(color);
+      }
+    });
+    updateColors(remainingColors);
   };
 
   return (
@@ -76,8 +111,40 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+      {/* <div className="spacer" /> */}
+
+    {/* stretch - build another form here to add a color */}
+
+      {editing && (
+        <form onSubmit={saveEdit}>
+          <legend>add color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorAddNew({ ...colorAddNew, color: e.target.value })
+              }
+              value={colorAddNew.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorAddNew({
+                  ...colorAddNew,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorAddNew.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">add</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,28 +1,6 @@
-// import React from "react";
-
-// const Login = () => {
-//   // make a post request to retrieve a token from the api
-//   // when you have handled the token, navigate to the BubblePage route
-//   return (
-//     <>
-//       <h1>Welcome to the Bubble App!</h1>
-//       <p>Build a login page here</p>
-
-//     </>
-//   );
-// };
-
-// export default Login;
-
-
 import React, { useState }from "react";
 import axiosWithAuth from "../utils/axiosWithAuth";
-
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
  
-
-
 class Login extends React.Component {
     state = {
       credentials: {
@@ -39,7 +17,8 @@ class Login extends React.Component {
         }
       });
     };
-  
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
     login = e => {
       e.preventDefault();
       axiosWithAuth()
@@ -54,23 +33,28 @@ class Login extends React.Component {
   
     render() {
       return ( 
-        <div>
+        <div className="login-container">
           <h1>Welcome to the Bubble App!</h1>
         <p>Build a login page here</p>
           <form onSubmit={this.login}>
+            <div className="login-input">
+            <h4>Username</h4>
             <input
               type="text"
               name="username"
               value={this.state.credentials.username}
               onChange={this.handleChange}
             />
+            <br></br>
+            <h4>Password</h4>
             <input
               type="password"
               name="password"
               value={this.state.credentials.password}
               onChange={this.handleChange}
             />
-            <button>Log in</button>
+            </div>
+            <button className="login-button">Log in</button>
           </form>
         </div>
       );
